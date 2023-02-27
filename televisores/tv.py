515003efeb8d796c71bd60a780ca297984196d11
Yiduaaar <124937930+Yiduaaar@users.noreply.github.com>
2023-02-26 23:09:28 -0500

    Create tv.py

---
+++ televisores/tv.py
@@ -0,0 +1,76 @@
+class TV:
+    numTV = 0
+
+    def _init_(self, marca, estado):
+        self.marca = marca
+        self.canal = 1
+        self.precio = 500
+        self.estado = estado
+        self.volumen = 1
+        self.control = None
+        TV.numTV +=1
+
+    @classmethod
+    def getNumTV(cls):
+        return cls.numTV
+    @classmethod
+    def setNumTV(cls,numTV):
+        cls.numTV = numTV
+        
+
+    def getMarca(self):
+        return self.marca
+
+    def setMarca(self, marca):
+        self.marca = marca
+
+    def getCanal(self):
+        return self.canal
+
+    def setCanal(self, canal):
+        if self.estado and 1 <= canal <= 120:
+            self.canal = canal
+
+    def getPrecio(self):
+        return self.precio
+
+    def setPrecio(self, precio):
+        self.precio = precio
+
+    def getEstado(self):
+        return self.estado
+
+    def turnOn(self):
+        self.estado = True
+
+    def turnOff(self):
+        self.estado = False
+
+    def canalUp(self):
+        if self.estado and self.canal < 120:
+            self.canal += 1
+
+    def canalDown(self):
+        if self.estado and self.canal > 1:
+            self.canal -= 1
+
+    def volumenUp(self):
+        if self.estado and self.volumen < 7:
+            self.volumen += 1
+
+    def volumenDown(self):
+        if self.estado and self.volumen > 0:
+            self.volumen -= 1
+
+    def getVolumen(self):
+        return self.volumen
+
+    def setVolumen(self, volumen):
+        if self.estado and 0 <= volumen <= 7:
+            self.volumen = volumen
+
+    def getControl(self):
+        return self.control
+
+    def setControl(self, control):
+        self.control = control
