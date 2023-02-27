f766bcd83d30adc939c0ba71e66d0b0da5cd2c1b
Yiduaaar <124937930+Yiduaaar@users.noreply.github.com>
2023-02-26 23:09:20 -0500

    Create control.py

---
+++ televisores/control.py
@@ -0,0 +1,41 @@
+class Control:
+    def _init_(self):
+        self.tv = None
+
+    def setTv(self, tv):
+        self.tv = tv
+
+    def getTv(self):
+        return self.tv
+
+    def turnOn(self):
+        if self.tv:
+            self.tv.turnOn()
+
+    def turnOff(self):
+        if self.tv:
+            self.tv.turnOff()
+
+    def canalUp(self):
+        if self.tv:
+            self.tv.canalUp()
+
+    def canalDown(self):
+        if self.tv:
+            self.tv.canalDown()
+
+    def volumenUp(self):
+        if self.tv:
+            self.tv.volumenUp()
+
+    def volumenDown(self):
+        if self.tv:
+            self.tv.volumenDown()
+
+    def setCanal(self, canal):
+        if self.tv:
+            self.tv.setCanal(canal)
+
+    def enlazar(self, tv):
+        self.tv = tv
+        tv.setControl(self)
